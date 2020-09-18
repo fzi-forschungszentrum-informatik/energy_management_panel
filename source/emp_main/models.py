5faf462a340fa77a3bb5207715aa1b6db217d95e
David Wölfle <woelfle@fzi.de>
2020-09-18 12:11:51 +0200

    Add datapoint model and corresponding tests

---
+++ source/emp_main/models.py
@@ -0,0 +1,195 @@
+import json
+
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Datapoint(models.Model):
+    """
+    Model for a datapoint.
+
+    A datapoint represents one source of information. Devices or Webservices
+    will typically emit information on more then one datapoints. E.g. climate
+    sensor in a room might publish temperature and humidity measurements. Both
+    will be treated as individual datapoints as this allows us to abstract
+    away the complexity of the devices.
+
+    TODO: May be replace delete with deactivate, else we will might end with
+          entries in the ValueDB with unknown origin (deleted datapoints will
+          be rentered but with new id)
+
+    TODO: Add consistency checks on save.
+    """
+
+    external_id = models.PositiveIntegerField(
+        null=True,
+        unique=True,
+        help_text=(
+            "This id used if Datapoints data is pushed in from an external tool"
+            " (e.g. BEMCom) that maintains it's own list datapoint metadata. "
+            "This field allows matching the ids of the external tools with "
+            "the database table of the EMP, effectively allowing the EMP "
+            "to store Datapoints additionally to what is pushed from the "
+            "external tool, e.g. for mock (fake) values."
+        )
+    )
+    TYPE_CHOICES = [
+        ("sensor", "Sensor"),
+        ("actuator", "Actuator"),
+    ]
+    type = models.CharField(
+        max_length=8,
+        default=None,
+        null=False,
+        choices=TYPE_CHOICES,
+        help_text=(
+            "Datapoint type, can be ether sensor or actuator."
+        )
+    )
+    # Defines the data format of the datapoint, i.e. which additional metadata
+    # we can expect to have reasonable values.
+    #
+    # The formats have the following meanings:
+    #   numeric: The value of the datapoint can be stored as a float.
+    #   text: The value of the datapoint can be stored as a string.
+    #   generic: No additional information.
+    #   continuous: The value is a continuous variable with an optional max
+    #               and min value, that can take any value in between.
+    #   discrete: The value of the datapoint can take one value of limited set
+    #             of possible values.
+    data_format_choices = [
+        ("generic_numeric", "Generic Numeric"),
+        ("continuous_numeric", "Continuous Numeric"),
+        ("discrete_numeric", "Discrete Numeric"),
+        ("generic_text", "Generic Text"),
+        ("discrete_text", "Discrete Text"),
+    ]
+    # Use generic_text as default as it imposes no constraints on the datapoint
+    # apart from that the value can be stored as string, which should always
+    # be possible as the value has been received as a JSON string.
+    data_format = models.CharField(
+        max_length=18,
+        choices=data_format_choices,
+        default="generic_text",
+        help_text=(
+            "Format of the datapoint value. Additionally defines which meta"
+            "data is available for it. See documentation in code for details."
+        )
+    )
+    # Don't limit this, people should never need to use abbreviations or
+    # shorten their thoughts just b/c the field is too short.
+    description = models.TextField(
+        editable=True,
+        blank=True,
+        help_text=(
+            "A human readable description of the datapoint targeted on "
+            "users of the API wihtout knowledge about connector details."
+        )
+    )
+    #
+    ##########################################################################
+    #
+    # Below all datapoint fields that may or may not be populated for a
+    # particular datapoint depending on the data_format and type.
+    #
+    ##########################################################################
+    #
+    last_value = models.TextField(
+        editable=True,
+        null=True,
+        help_text=(
+            "The last value received for the datapoint. We store all values "
+            "including numeric as strings as this simplfies the logic "
+            "significantly and prevents unintended side effects, e.g. data "
+            "loss if the data format field is changed."
+            ""
+        )
+    )
+    last_value_timestamp = models.DateTimeField(
+        editable=True,
+        null=True,
+        help_text=(
+            "The timestamp of the last value received via MQTT."
+        )
+    )
+    last_setpoint = models.TextField(
+        editable=True,
+        null=True,
+        help_text=(
+            "The last schedule received for the datapoint. "
+            "Applicable to actuator datapoints."
+        )
+    )
+    last_setpoint_timestamp = models.DateTimeField(
+        editable=True,
+        null=True,
+        help_text=(
+            "The timestamp of the last value received for the datapoint."
+            "Applicable to actuator datapoints."
+        )
+    )
+    last_schedule = models.TextField(
+        editable=True,
+        null=True,
+        help_text=(
+            "The last schedule received for the datapoint."
+            "Applicable to actuator datapoints."
+        )
+    )
+    last_schedule_timestamp = models.DateTimeField(
+        editable=True,
+        null=True,
+        help_text=(
+            "The timestamp of the last value received for the datapoint."
+            "Applicable to actuator datapoints."
+        )
+    )
+    allowed_values = models.TextField(
+        blank=False,
+        null=False,
+        default="[]",
+        help_text=(
+            "Allowed values. Applicable to discrete valued datapoints. "
+            "Must be a valid JSON string."
+        )
+    )
+    min_value = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        help_text=(
+            "The minimal expected value of the datapoint. "
+            "Applicable to numeric datapoints."
+        )
+    )
+    max_value = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        help_text=(
+            "The maximal expected value of the datapoint. "
+            "Applicable to numeric datapoints."
+        )
+    )
+    unit = models.TextField(
+        editable=True,
+        default="",
+        blank=True,
+        help_text=(
+            "The unit in SI notation, e.g.  Mg*m*s^-2 aka. kN. "
+            "Applicable to numeric datapoints."
+        )
+    )
+
+    def __str__(self):
+        return (str(self.id) + " - " + self.description)
+
+    def clean(self):
+        """
+        """
+        try:
+            _ = json.loads(self.allowed_values)
+        except Exception:
+            raise ValidationError(
+                "Allowed values contains no valid JSON string."
+            )
---
+++ source/emp_main/tests/test_models.py
@@ -0,0 +1,232 @@
+import json
+
+from django.test import TestCase
+from django.db.utils import IntegrityError
+
+from ..models import Datapoint
+from ..utils import datetime_from_timestamp
+
+class TestDemoAppPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Generic default values to prevent errors when creating datapoints
+        # while violating non empty constraints.
+        cls.default_field_values = {"type": "sensor"}
+
+
+    def generic_field_value_test(self, field_values):
+        """
+        Create a datapoint with field_values, and check that the value can be
+        restored.
+        """
+        datapoint = Datapoint.objects.create(**field_values)
+        datapoint.save()
+        # Ensure that we compare to the value that has been stored in DB.
+        datapoint.refresh_from_db()
+        for field in field_values:
+            expected_value = field_values[field]
+            actual_value = getattr(datapoint, field)
+            self.assertEqual(expected_value, actual_value)
+
+    def test_field_external_id_exists(self):
+        """
+        This field is required to match externally managed datapoint metadata.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"external_id": 1})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_external_id_is_unique(self):
+        """
+        external_id must be unique as we use it to select a single datapoint
+        which is updated with the data from the external system.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"external_id": 1})
+
+        self.generic_field_value_test(field_values=field_values)
+        with self.assertRaises(IntegrityError):
+            self.generic_field_value_test(field_values=field_values)
+
+    def test_field_external_id_is_not_unique_when_null(self):
+        """
+        Verify that we can have several datapoints with all null as external_id
+        as this will happen if we add additional datapoints locally while others
+        exist that are pushed from the external system.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"external_id": None})
+
+        self.generic_field_value_test(field_values=field_values)
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_type_exists(self):
+        """
+        This field is an essential information about the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"type": "actuator"})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_data_format_exists(self):
+        """
+        This field is an essential information about the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"data_format": "generic_numeric"})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_decription_exists(self):
+        """
+        This field is an essential information about the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"description": "Sample text"})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_last_value_exists(self):
+        """
+        This field stores datapoint payload.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"last_value": "3.12345"})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_last_value_timestamp_exists(self):
+        """
+        This field stores datapoint payload.
+        """
+        field_values = self.default_field_values.copy()
+
+        ts = 1596240000000
+        ts_datetime = datetime_from_timestamp(ts, tz_aware=True)
+        field_values.update({"last_value_timestamp": ts_datetime})
+
+        self.generic_field_value_test(field_values=field_values)
+
+
+    def test_last_setpoint_exists(self):
+        """
+        This field stores datapoint payload.
+
+        TODO: Add realistic setpoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        last_setpoint = json.dumps([
+            {
+                'from_timestamp': None,
+                'to_timestamp': 1564489613491,
+                'preferred_value': 21,
+            },
+            {
+                'from_timestamp': 1564489613491,
+                'to_timestamp': None,
+                'preferred_value': None,
+            }
+        ])
+        field_values.update({"last_setpoint": last_setpoint})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_last_setpoint_timestamp_exists(self):
+        """
+        This field stores datapoint payload.
+        """
+        field_values = self.default_field_values.copy()
+
+        ts = 1596240000000
+        ts_datetime = datetime_from_timestamp(ts, tz_aware=True)
+        field_values.update({"last_setpoint_timestamp": ts_datetime})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_last_schedule_exists(self):
+        """
+        This field stores datapoint payload.
+
+        TODO: Add realistic schedule.
+        """
+        field_values = self.default_field_values.copy()
+
+        last_schedule = json.dumps([
+            {
+                'from_timestamp': None,
+                'to_timestamp': 1564489613491,
+                'value': 21
+            },
+            {
+                'from_timestamp': 1564489613491,
+                'to_timestamp': None,
+                'value': None
+            }
+        ])
+        field_values.update({"last_schedule": last_schedule})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_last_schedule_timestamp_exists(self):
+        """
+        This field stores datapoint payload.
+        """
+        field_values = self.default_field_values.copy()
+
+        ts = 1596240000000
+        ts_datetime = datetime_from_timestamp(ts, tz_aware=True)
+        field_values.update({"last_schedule_timestamp": ts_datetime})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_allowed_values_exists(self):
+        """
+        This field carries additional metadata on the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        allowed_values = json.dumps([1, 2, 3])
+        field_values.update({"allowed_values": allowed_values})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_min_value_exists(self):
+        """
+        This field carries additional metadata on the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"min_value": 10.0})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_field_max_value_exists(self):
+        """
+        This field carries additional metadata on the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"max_value": 10.0})
+
+        self.generic_field_value_test(field_values=field_values)
+
+    def test_unit_exists(self):
+        """
+        This field carries additional metadata on the datapoint.
+        """
+        field_values = self.default_field_values.copy()
+
+        field_values.update({"unit": "Mg*m*s^-2"})
+
+        self.generic_field_value_test(field_values=field_values)
