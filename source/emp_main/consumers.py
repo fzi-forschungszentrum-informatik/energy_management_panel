9f44f231f85867c72bf7a27134b353fb32ff9d5f
David Wölfle <woelfle@fzi.de>
2020-10-06 11:16:30 +0200

    Add Websocket consumer to send datapoint updates.

---
+++ source/emp_main/consumers.py
@@ -0,0 +1,123 @@
+import json
+import logging
+from urllib import parse as urlparse
+from datetime import datetime
+
+from django.db.models.signals import post_save
+from channels.generic.websocket import JsonWebsocketConsumer
+
+from .models import Datapoint
+from .apps import EmpAppsCache
+from .utils import datetime_to_pretty_str
+
+logger = logging.getLogger(__name__)
+
+
+class DatapointUpdate(JsonWebsocketConsumer):
+    """
+    A consumer that sends updates of Datapoint fields via websocket to the
+    user.
+
+    Manuelly test with:
+    Execute in browser development console:
+        ws = new WebSocket("ws://localhost:8000/ws/datapoint-update/?datapoint-ids=[1,2]");
+        ws.onmessage = function(msg){console.log(JSON.parse(msg.data))}
+    Change the value of http://localhost:8000/admin/emp_main/datapoint/
+    in the admin panel.
+    """
+
+    def connect(self):
+        """
+        Accept every connection and compute the ids of datapoints the user is
+        permitted to receive updates for.
+        """
+        self.user = self.scope["user"]
+        self.accept()
+        logger.debug(
+            "DatapointUpdate consumer accepted connection from user=%s",
+            self.user
+        )
+
+        # Parse the set of requested datapoints from the query string.
+        try:
+            query_string = self.scope["query_string"].decode('utf8')
+            query_string_parsed = urlparse.parse_qs(query_string)
+            # if the query string looks like this: ?datapoint-ids=[1,2]"
+            # then the query_string_parsed object looks like this:
+            # {'datapoint-ids': ['[1,2]']}
+            datapoint_ids_qs_json = query_string_parsed["datapoint-ids"][0]
+            requested_dp_ids = set(json.loads(datapoint_ids_qs_json))
+        except:
+            logging.exception(
+                "DatapointUpdate consumer received object not translatable "
+                "into requested datapoint ids by user=%s.",
+                self.user
+            )
+            raise
+
+        # Compute the set of all datapoint ids for which the user has
+        # permissions.
+        apps_cache = EmpAppsCache.get_instance()
+        dp_ids = apps_cache.get_allowed_datapoint_ids_for_user(self.user)
+        all_allowed_dp_ids = set(dp_ids)
+
+        # Update permitted ids to trigger that updates for those datapoints
+        # are forwarded to the client.
+        permitted_ids = all_allowed_dp_ids.intersection(requested_dp_ids)
+        self.permitted_datapoint_ids = permitted_ids
+        logger.debug(
+            "DatapointUpdate consumer updated permitted ids for user=%s "
+            "to datapoints=%s",
+            *(self.user, permitted_ids)
+        )
+
+        # Check if the client requested ids he has no permissions for.
+        # This should usually not happen if a user opens a normal web page
+        # but only if people start playing around with the websocket manually.
+        denied_ids = requested_dp_ids.difference(all_allowed_dp_ids)
+        if denied_ids:
+            logger.warning(
+                "DatapointUpdate consumer denied user=%s access to the"
+                "datapoints=%s",
+                *(self.user, denied_ids)
+            )
+
+        # Connect to datapoint signal so this consumer can send the updated
+        # data of that datapoint to the user. Use a dispatch uid for signals,
+        # so we disconnect from the correct signal on disconnect.
+        self.post_save_uid = "post_save" + str(id(self))
+        post_save.connect(
+            self.send_datapoint_update_to_client,
+            sender=Datapoint,
+            dispatch_uid=self.post_save_uid
+        )
+
+    def send_datapoint_update_to_client(self, sender, **kwargs):
+        """
+        Sends a JSON version of a changed datapoint via Websocket to the
+        lient.
+
+        TODO: Use a proper serializer.
+        """
+        logger.debug("DatapointUpdate consumer got datapoint update %s", kwargs)
+        datapoint = kwargs["instance"]
+        if datapoint.id not in self.permitted_datapoint_ids:
+            return
+        dp_field_values = {}
+        for field in datapoint._meta.fields:
+            field_value = getattr(datapoint, field.name)
+            # JSON can't carry datetime objects.
+            if isinstance(field_value, datetime):
+                field_value = datetime_to_pretty_str(field_value)
+
+            dp_field_values[field.name] = field_value
+        self.send_json(dp_field_values)
+
+
+    def disconnect(self, close_code):
+        """
+        Disconnect from signal.
+        """
+        post_save.disconnect(
+            dispatch_uid=self.post_save_uid
+        )
