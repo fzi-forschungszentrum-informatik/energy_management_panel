46cef18fb459ba185243e3c81cdafdeb0bc9801a
David Wölfle <woelfle@fzi.de>
2022-04-19 21:56:00 +0200

    Remove SQLite File.
    Is not required anymore, will be populated from fixture on startup.

--- source/emp/emp_main/settings.py
+++ source/emp/emp_main/settings.py
@@ -145,7 +145,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR.parent / "db.sqlite3",
         }
     }
 
