7e45c26775c294b90e3eae5e3ee0e2dc5b8efc62
David Wölfle <woelfle@fzi.de>
2022-05-10 23:28:00 +0200

    Remove Anon user permissions. It is redundant with settings.URLS_PERMISSION_WHITELIST and hard to update.

--- source/emp/emp_demo_ui_app/apps.py
+++ source/emp/emp_demo_ui_app/apps.py
@@ -45,20 +45,15 @@ def get_app_nav_content_for_user(user):
     # is important, then it can be defined here explictly.
     app_nav_content = OrderedDict()
 
-    # Get the anonymous user object.
-    anon = get_user_model().get_anonymous()
-
-    # Get the pages for the user and extend these with the pages the
-    # anonymous user has access to.
+    # Get the pages the user has access too.
     pages_user = get_objects_for_user(user, "emp_demo_ui_app.view_demoapppage")
-    pages_anon = get_objects_for_user(anon, "emp_demo_ui_app.view_demoapppage")
-    pages_all = pages_user.union(pages_anon).order_by("page_name")
+    pages_user = pages_user.order_by("page_name")
 
     # Be sure to use slugs here for url components.
     # Also ensure that any user gets a copy of the pages the AU gets and
     # that inactive users get not more permissions then the AU.
     app_pages = OrderedDict()
-    for page_obj in pages_all:
+    for page_obj in pages_user:
         app_pages[page_obj.page_name] = page_obj.get_absolute_url()
     app_nav_content["Demo UI App"] = app_pages
 
@@ -87,17 +82,11 @@ def get_permitted_datapoint_ids_for_user(user):
     # This import must be here as it cannot succeed until all apps are loaded.
     from guardian.shortcuts import get_objects_for_user
 
-    # Get the anonymous user object.
-    anon = get_user_model().get_anonymous()
-
-    # Get the pages for the user and extend these with the pages the
-    # anonymous user has access to.
+    # Get the pages for the user.
     pages_user = get_objects_for_user(user, "emp_demo_ui_app.view_demoapppage")
-    pages_anon = get_objects_for_user(anon, "emp_demo_ui_app.view_demoapppage")
-    pages_all = pages_user.union(pages_anon)
 
     datapoint_ids = set()
-    for page in pages_all:
+    for page in pages_user:
         if page.demo_datapoint is not None:
             datapoint_ids.add(page.demo_datapoint.id)
     return datapoint_ids
